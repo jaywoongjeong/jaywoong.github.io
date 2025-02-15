960e459fca1d3d96f90554f759edb14d7d05337e
jaywoongjeong <jaywoong.jeong@gmail.com>
2025-02-14 19:20:31 -0500

    Update next.config.ts: remove basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: '', // 이전에 '/jaywoong.github.io'로 되어있었을 수 있음
+    basePath: '', // 완전히 비워두기
     images: {
         unoptimized: true
     }
